7f1199572b28847ac140cfd55677ba26c217b49a
0sAf456 <ainaras448@gmail.com>
2025-03-11 16:36:01 +0200

    i wont even tell how much time i spend with this shit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,20 @@
 <script>
 	import '../app.css';
+	import 'typeface-roboto';
 	let { children } = $props();
 </script>
 
-<div class="min-h-screen bg-black">
-	<div class=" flex items-center justify-center p-6">
-		<nav class=" w-auto rounded-2xl border-1 bg-black p-4 text-center text-xl font-bold text-white">
-			<div class="items-center justify-center text-center">
-				<a class="rounded-2xl border-0 p-3 hover:bg-gray-400" href="/">Namai namuciai </a>
-				<a class=" rounded-2xl border-0 p-3 hover:bg-gray-400" href="/about"
-					>Mentas kad klausineji APIE MANE</a
-				>
-				<a class=" rounded-2xl border-0 p-3 hover:bg-gray-400" href="/projects"
-					>Dabar gerai pagalvok ar ktu studentas tures PROJEKTU</a
-				>
-				<a class="rounded-2xl border-0 p-3 hover:bg-gray-400" href="/contact">tap in</a>
-			</div>
-		</nav>
-	</div>
+<div class=" flex items-center justify-center bg-black p-10">
+	<nav
+		class=" mb-5 w-auto rounded-2xl border-1 bg-black p-4 text-center text-xl font-bold text-white"
+	>
+		<div class="items-center justify-center text-center">
+			<a class="rounded-2xl border-0 p-3 hover:bg-gray-400" href="/">Home</a>
+			<a class=" rounded-2xl border-0 p-3 hover:bg-gray-400" href="/about">About</a>
+			<a class=" rounded-2xl border-0 p-3 hover:bg-gray-400" href="/projects">Projects</a>
+			<a class="rounded-2xl border-0 p-3 hover:bg-gray-400" href="/contact">Tap in</a>
+		</div>
+	</nav>
 </div>
+
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,50 @@
 	import '../app.css';
 </script>
 
-<h1 class="text-3xl text-blue-500">Namai namuciai</h1>
-<h1 class="">Hi Ainaras there</h1>
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<div class="min-h-screen bg-black">
+	<div class="m-auto max-w-4/12">
+		<div class="flex justify-start gap-4">
+			<h1 class="mb-7 text-5xl font-bold text-white">Hi, Ainaras there</h1>
+			<h1 class="hover:animate-wave mb-7 text-5xl">👋</h1>
+		</div>
+		<div class="flex items-start gap-8">
+			<div>
+				<p class="mb-5 text-xl text-white">20 year old student based in Kaunas, Lithuania</p>
+				<p class=" text-xl text-white">
+					Looking to be hired in this is massive tech world that we are in, intrested in full stack
+					development and the tech world around us
+				</p>
+			</div>
+
+			<img src="/face.jpg" alt="My face" class="h-[200px] w-[200px] rounded-2xl" />
+		</div>
+	</div>
+</div>
+
+<style>
+	@keyframes wave {
+		0% {
+			transform: rotate(0deg);
+		}
+		25% {
+			transform: rotate(15deg);
+		}
+		50% {
+			transform: rotate(0deg);
+		}
+		75% {
+			transform: rotate(-15deg);
+		}
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+
+	.hover\:animate-wave:hover {
+		animation: wave 0.5s ease-in-out;
+	}
+</style>
